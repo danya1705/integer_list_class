c3651e6ffd4c0dd014bb54525d15e7b21649e854
danya1705 <danya17@gmail.com>
2022-10-28 16:05:38 +0300

    first commit

---
+++ src/main/java/pro/sky/java/course2/integer_list_class/IntegerListClassApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.integer_list_class;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegerListClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegerListClassApplication.class, args);
+    }
+
+}
